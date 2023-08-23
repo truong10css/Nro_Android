6d3cd66b67a2c498e7243428efd9fad6e701081d
truong10css <113449308+truong10css@users.noreply.github.com>
2023-08-23 17:36:03 +0700

    Update nrotruong.py

--- nrotruong.py
+++ nrotruong.py
@@ -2,6 +2,7 @@ import base64
 import os
 import shutil
 import time
+import zipfile
 
 print(base64.b64decode('LS0tLS1Ub29sIEVuY29kZSBCeSBNWFQtLS0tLQ==').decode())
 time.sleep(2)
@@ -16,9 +17,8 @@ while True:
     print("╚═╝░░░░░╚═╝░░░╚═╝░░░░░░╚═╝░░░")
     
     print("\033[1;96m[1] Kiểm tra và cài đặt OpenJDK 17")
-    print("\033[1;932m[2] Sao chép tệp từ điện thoại và tạo thư mục 'nro'")
-    print("\033[1;94m[3] Chạy server")
-    print("\033[1;91m[4] Thoát")
+    print("\033[1;932m[2] Sao chép tệp từ điện thoại vào Termux, giải nén và xóa tệp")
+    print("\033[1;91m[3] Thoát")
     
     luachon = input("\033[1;92mLựa chọn: ")
     
@@ -35,31 +35,26 @@ while True:
         continue
     elif luachon == '2':
         time.sleep(1)
-        src_zip_path = '/sdcard/Download/nro.zip'
-        unzip_dir = '/data/data/com.termux/files/home/nro_folder/'
+        src_file = 'sdcard/Download/mad3.zip'
+        dest_folder = '/data/data/com.termux/files/home/nro'
         
-        if os.path.exists(unzip_dir):
-            print("Đã tồn tại folder rồi!")
-        else:
-            os.makedirs(unzip_dir, exist_ok=True)
-            shutil.copy(src_zip_path, unzip_dir)
-            os.chdir(unzip_dir)
-            os.system('unzip mad3.zip && clear')
-            os.system('rm -rf mad3.zip')
-            os.system('clear')
-            print("\033[1;92mĐã sao chép thành công từ điện thoại và tạo thư mục 'nro'.\n")
-          continue
+        try:
+            os.makedirs(dest_folder, exist_ok=True)
+            shutil.copy2(src_file, dest_folder)
+            print("\033[1;92mĐã sao chép thành công từ điện thoại vào Termux.\n")
+            
+            with zipfile.ZipFile(os.path.join(dest_folder, 'mad3.zip'), 'r') as zip_ref:
+                zip_ref.extractall(dest_folder)
+                print("\033[1;92mĐã giải nén tệp.\n")
+            
+            os.remove(os.path.join(dest_folder, 'mad3.zip'))
+            print("\033[1;92mĐã xóa tệp sau khi giải nén.\n")
+            
+        except Exception as e:
+            print("\033[1;91mĐã xảy ra lỗi: {}\n".format(e))
+        
+        continue
     elif luachon == '3':
-        time.sleep(1)
-        # Kiểm tra thư mục đã giải nén
-        if os.path.exists('/data/data/com.termux/files/home/nro/dist/mad.jar'):
-            print("\033[1;35mĐang khởi động máy chủ...")
-            os.system('java -jar /data/data/com.termux/files/home/nro/dist/mad.jar')
-        else:
-            print("\033[1;91mThư mục 'nro' chưa được giải nén. Vui lòng thực hiện bước 2 trước.\n")
-        
-        break
-    elif luachon == '4':
         print("\033[1;91mĐã thoát chương trình.")
         break
     else:
