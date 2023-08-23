41fc21461952252f1e60a667a5dd622f1aa8d72f
truong10css <113449308+truong10css@users.noreply.github.com>
2023-08-23 17:48:44 +0700

    Update nrotruong.py

--- nrotruong.py
+++ nrotruong.py
@@ -4,10 +4,14 @@ import shutil
 import time
 import zipfile
 
-print(base64.b64decode('LS0tLS1Ub29sIEVuY29kZSBCeSBNWFQtLS0tLQ==').decode())
+def clear_screen():
+    os.system('clear')
+
+print("\033[1;91m HI!IAM QUANG TRUONG.")
 time.sleep(2)
 
 while True:
+    clear_screen()
     print("\033[1;92m")
     print("███╗░░░███╗░██████╗░████████╗")
     print("████╗░████║██╔═══██╗╚══██╔══╝")
@@ -18,11 +22,13 @@ while True:
     
     print("\033[1;96m[1] Kiểm tra và cài đặt OpenJDK 17")
     print("\033[1;932m[2] Sao chép tệp từ điện thoại vào Termux, giải nén và xóa tệp")
-    print("\033[1;91m[3] Thoát")
+    print("\033[1;94m[3] Chạy server")
+    print("\033[1;91m[4] Thoát")
     
     luachon = input("\033[1;92mLựa chọn: ")
     
     if luachon == '1':
+        clear_screen()
         time.sleep(1)
         # Kiểm tra cài đặt OpenJDK 17
         result = os.system('java -version 2>&1 | grep "openjdk version" | grep "17"')
@@ -32,10 +38,12 @@ while True:
             os.system('pkg install openjdk-17 -y -y')
             print("\033[1;92mCài đặt OpenJDK 17 thành công.\n")
         
+        input("\033[1;92mNhấn Enter để tiếp tục...")
         continue
     elif luachon == '2':
+        clear_screen()
         time.sleep(1)
-        src_file = 'sdcard/Download/mad3.zip'
+        src_file = '/storage/emulated/0/Download/mad3.zip'
         dest_folder = '/data/data/com.termux/files/home/nro'
         
         try:
@@ -43,22 +51,39 @@ while True:
             shutil.copy2(src_file, dest_folder)
             print("\033[1;92mĐã sao chép thành công từ điện thoại vào Termux.\n")
             
-            with zipfile.ZipFile(os.path.join(dest_folder, 'mad3.zip'), 'r') as zip_ref:
-                zip_ref.extractall(dest_folder)
-                print("\033[1;92mĐã giải nén tệp.\n")
-            
-            os.remove(os.path.join(dest_folder, 'mad3.zip'))
-            print("\033[1;92mĐã xóa tệp sau khi giải nén.\n")
+            if not os.path.exists(os.path.join(dest_folder, 'dist/mad.jar')):
+                with zipfile.ZipFile(os.path.join(dest_folder, 'mad3.zip'), 'r') as zip_ref:
+                    zip_ref.extractall(dest_folder)
+                    print("\033[1;92mĐã giải nén tệp.\n")
+                
+                os.remove(os.path.join(dest_folder, 'mad3.zip'))
+                print("\033[1;92mĐã xóa tệp sau khi giải nén.\n")
+            else:
+                print("\033[1;91mThư mục đã giải nén tồn tại.\n")
             
         except Exception as e:
             print("\033[1;91mĐã xảy ra lỗi: {}\n".format(e))
         
+        input("\033[1;92mNhấn Enter để tiếp tục...")
         continue
     elif luachon == '3':
+        clear_screen()
+        time.sleep(1)
+        if os.path.exists('/data/data/com.termux/files/home/nro/dist/mad.jar'):
+            print("\033[1;35mĐang khởi động máy chủ...")
+            os.system('java -Xms2G -Xmx2G -jar /data/data/com.termux/files/home/nro/dist/mad.jar')
+        else:
+            print("\033[1;91mThư mục 'nro' chưa được giải nén. Vui lòng thực hiện bước 2 trước.\n")
+        
+        input("\033[1;92mNhấn Enter để tiếp tục...")
+        break
+    elif luachon == '4':
+        clear_screen()
         print("\033[1;91mĐã thoát chương trình.")
         break
     else:
+        clear_screen()
         print("\033[1;91mLựa chọn không hợp lệ. Vui lòng chọn lại.")
         print("\033[1;93m-------------------------------------------------------------")
-        time.sleep(1)
+        input("\033[1;92mNhấn Enter để tiếp tục...")
         continue
