0e6a2f35b5a0c4c56f556d497de0e40452fb943c
truong10css <113449308+truong10css@users.noreply.github.com>
2023-08-24 12:57:39 +0700

    Update nrotruong.py

--- nrotruong.py
+++ nrotruong.py
@@ -8,8 +8,8 @@ def clear_screen():
     os.system('clear')
 
 def install_ngrok():
-    os.system('wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm-asia.zip
-    os.system('unzip ngrok-stable-linux-arm-asia.zip')
+    os.system('"wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip"')
+    os.system('unzip ngrok-stable-linux-arm.zip')
 
 def start_ngrok_tcp(auth_token):
     os.system('./ngrok authtoken {}'.format(auth_token))
